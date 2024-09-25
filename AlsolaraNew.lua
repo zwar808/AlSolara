cbdaa25bd76c033ee03f6e5820ee202a9f2065ff
galaxyvape <starsspacealtyeah@gmail.com>
2024-09-25 02:55:16 -0700

    Update AlsolaraNew.lua

--- AlsolaraNew.lua
+++ AlsolaraNew.lua
@@ -1,10 +1,9 @@
--- Beta testing not done
-loadstring(game:HttpGet("https://raw.githubusercontent.com/zwar808/BetterUNC/refs/heads/main/lib/require.lua"))()
+local requireLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/zwar808/BetterUNC/refs/heads/main/lib/require.lua"))()
 local debug = loadstring(game:HttpGet("https://raw.githubusercontent.com/zwar808/BetterUNC/refs/heads/main/lib/Debug.lua"))()
 local Drawing = loadstring(game:HttpGet("https://raw.githubusercontent.com/zwar808/BetterUNC/refs/heads/main/lib/Drawing.lua"))()
 local net = loadstring(game:HttpGet("https://raw.githubusercontent.com/zwar808/BetterUNC/refs/heads/main/lib/Net.lua"))()
 
 if debug then
-	loadstring(game:HttpGet("https://github.com/zwar808/AlSolara/raw/refs/heads/main/Alsploit/Modified.lua",true))()
-	print("[Debug] Operating: Alsploit")
+    loadstring(game:HttpGet("https://github.com/zwar808/AlSolara/raw/refs/heads/main/Alsploit/Modified.lua", true))()
+    print("[Debug] Operating: Alsploit")
 end
