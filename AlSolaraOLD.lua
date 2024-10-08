a3dc6be64e856790d939dc5cef2f6ab9488f348b
Liquid <iloveexploiting123@gmail.com>
2024-10-08 16:38:40 +0800

    Update AlSolaraOLD.lua

--- AlSolaraOLD.lua
+++ AlSolaraOLD.lua
@@ -1,73 +1,401 @@
-local fix = Instance.new("ScreenGui", game.Players.LocalPlayer:WaitForChild("PlayerGui"))
-fix.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+-- Gui to Lua
+-- Version: 3.2
 
-local Frame = Instance.new("Frame", fix)
-Frame.BackgroundColor3 = Color3.fromRGB(34, 34, 34)
-Frame.Position = UDim2.new(0.262, 0, 0.223, 0)
-Frame.Size = UDim2.new(0, 646, 0, 334)
-Frame.Visible = false
+-- Instances:
 
-local function createTextLabel(parent, position, size, text)
-    local label = Instance.new("TextLabel", parent)
-    label.BackgroundTransparency = 1
-    label.Position = position
-    label.Size = size
-    label.Font = Enum.Font.Unknown
-    label.TextColor3 = Color3.fromRGB(255, 255, 255)
-    label.TextScaled = true
-    label.Text = text
-    return label
+local CleanGui = Instance.new("ScreenGui")
+local BlurTextDark = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local TextLabel = Instance.new("TextLabel")
+local TextLabel_2 = Instance.new("TextLabel")
+
+--Properties:
+
+CleanGui.Name = "CleanGui"
+CleanGui.Parent = game.StarterGui["Cr1tcal3's GUI toolkit"]
+CleanGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+BlurTextDark.Name = "BlurTextDark"
+BlurTextDark.Parent = CleanGui
+BlurTextDark.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
+BlurTextDark.BackgroundTransparency = 0.350
+BlurTextDark.BorderColor3 = Color3.fromRGB(0, 0, 0)
+BlurTextDark.BorderSizePixel = 0
+BlurTextDark.Position = UDim2.new(-1, 0, 0, 0)
+BlurTextDark.Size = UDim2.new(0, 427, 0, 605)
+
+UICorner.Parent = BlurTextDark
+
+TextLabel.Parent = BlurTextDark
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(0.0562661961, 0, 0.0423958264, 0)
+TextLabel.Size = UDim2.new(0, 381, 0, 281)
+TextLabel.Font = Enum.Font.Unknown
+TextLabel.Text = "Hey everyone, unfortunately, <font color=\"#FF8C00\"><b>ALSploit Solara</b></font> is shutting down, <b>maybe for good</b>.<br />I've got a lot going on in my life right now, and I hope you all understand.<br />Maybe <font color=\"#FF8C00\"><b>ALSploit Solara</b></font> will come back in December or on <b>October 31st</b>. It depends.<br /><font color=\"#FF8C00\"><b>Thanks for everything</b></font>.\\n"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextSize = 27.000
+TextLabel.TextWrapped = true
+TextLabel.TextXAlignment = Enum.TextXAlignment.Left
+TextLabel.TextYAlignment = Enum.TextYAlignment.Top
+
+TextLabel_2.Parent = BlurTextDark
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.BackgroundTransparency = 1.000
+TextLabel_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.BorderSizePixel = 0
+TextLabel_2.Position = UDim2.new(0.57617259, 0, 0.472147882, 0)
+TextLabel_2.Size = UDim2.new(0, 116, 0, 33)
+TextLabel_2.Font = Enum.Font.Unknown
+TextLabel_2.Text = "-cr1tcal3"
+TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.TextSize = 27.000
+TextLabel_2.TextWrapped = true
+TextLabel_2.TextXAlignment = Enum.TextXAlignment.Left
+TextLabel_2.TextYAlignment = Enum.TextYAlignment.Top
+
+-- Scripts:
+
+local function PPQLU_fake_script() -- BlurTextDark.BlurCreator 
+	local script = Instance.new('LocalScript', BlurTextDark)
+
+	_=[[
+	
+	⣿⣿⣿⣿⡿⠟⠛⠋⠉⠉⠉⠉⠉⠛⠛⠻⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+	⣿⣿⠟⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠈⠙⠾⣿⣾⣿⣾⣿⣾⣿⣾⣿
+	⠋⡁⠀⠀⠀⠀⠀⢀⠔⠁⠀⠀⢀⠠⠐⠈⠁⠀⠀⠁⠀⠈⠻⢾⣿⣾⣿⣾⣟⣿
+	⠊⠀⠀⠀⠀⢀⠔⠃⠀⠀⠠⠈⠁⠀⠀⠀⠀⠀⠀⠆⠀⠀⠄⠀⠙⣾⣷⣿⢿⣿
+	⠀⠀⠀⠀⡠⠉⠀⠀⠀⠀⠠⢰⢀⠀⠀⠀⠀⠀⠀⢰⠀⠀⠈⡀⠀⠈⢿⣟⣿⣿
+	⠀⠀⢀⡜⣐⠃⠀⠀⠀⣠⠁⡄⠰⠀⠀⠀⠀⠀⠀⠐⠀⠀⠀⠰⠀⠀⠈⢿⣿⣿
+	⠀⢠⠆⢠⡃⠀⠀⠀⣔⠆⡘⡇⢘⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿
+	⢀⡆⠀⡼⢣⠀⢀⠌⢸⢠⠇⡇⢘⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿
+	⣼⣃⠀⠁⢸⢀⠎⠀⢸⠎⠀⢸⢸⡄⠀⠀⠀⠀⠀⠂⢀⠀⠀⠀⠀⠀⠀⠀⠀⣿
+	⠃⡏⠟⣷⣤⠁⠀⠀⠸⠀⠀⡾⢀⢇⠀⠀⠀⠀⠀⠄⠸⠀⠀⠀⠀⠄⠀⠀⠀⣿
+	⠀⠀⣀⣿⣿⣿⢦⠀⠀⠀⠀⡧⠋⠘⡄⠀⠀⠀⠀⡇⢸⠀⠀⠠⡘⠀⠀⠀⢠⣿
+	⠈⠀⢿⢗⡻⠃⠀⠀⠀⠀⠀⠀⠀⠀⠱⡀⠀⠀⢰⠁⡇⠀⠀⢨⠃⡄⢀⠀⣸⣿
+	⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣱⠀⠀⡎⠸⠁⠀⢀⠞⡸⠀⡜⢠⣿⣿
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣺⣿⣧⢰⣧⡁⡄⠀⡞⠰⠁⡸⣠⣿⣿⣿
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⡿⠏⣿⠟⢁⠾⢛⣧⢼⠁⠀⠀⢰⣿⡿⣷⣿
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠡⠄⠀⡠⣚⡷⠊⠀⠀⠀⣿⡿⣿⡿⣿
+	⡀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠊⠁⢸⠁⠀⠀⠀⢰⣿⣿⡿⣿⣿
+	⠱⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡠⠊⠀⠀⠀⡞⠀⠀⠀⠀⢸⣿⣷⣿⣿⣿
+	⠀⠙⢦⣀⠀⠀⠀⠀⠀⢀⣀⣠⠖⠁⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⣸⣿⣾⡿⣷⣿
+	⠀⠀⠀⠀⠉⠉⣩⡞⠉⠁⠀⢸⡄⠀⠀⠀⠀⠀⢰⠇⠀⠀⠀⠀⣿⣿⣷⣿⣿⣿
+	⡆⠀⠀⣀⡠⠞⠁⣧⢤⣀⣀⣀⡇⠀⠀⠀⠀⠀⣸⠀⠀⠀⠀⠀⣿⣷⣿⣷⣿⣿
+	⣿⣷⠊⠁⠀⠀⡰⠁⠀⠀⠀⠀⣹⠶⢦⡀⠀⠀⡇⠀⠀⠀⠀⠀⢸⣿⣷⣿⣷⣿
+	⣿⢿⠀⠀⠀⡔⠁⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⡄⡇⠀⠀⠀⠀⠀⠈⣿⣾⣷⣿⣿
+	⠋⠈⠀⢀⠜⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠈⣧⠀⠀⠀⠀⠀⠀⠻⣿⣽⣾⣿
+	⢀⡄⡠⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠁⠀⠀⠀⣸⠀⠀⠀⠀⠀⠀⠀⣿⣿⣻⣿
+	⣿⠋⠀⠀⠀⠀⠀⠀⠀⠀⢰⠀⠐⠀⠀⠀⠀⣀⡿⠀⠀⠀⠀⠀⠀⠀⢹⣿⣻⣿
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼⠀⠀⠀⠀⠀⢀⣃⡇⠀⠲⡀⠀⠀⠀⠀⠈⣿⡿⣿
+	⣀⠤⠤⠤⡀⠀⠀⠀⠀⡴⠃⠀⠀⠀⠀⠀⢬⠞⡇⠀⠀⠇⠀⠀⠀⠀⠀⣿⣿⣿
+	⡁⢀⠀⠀⡇⠀⠀⠀⡼⠁⠀⠀⠀⠀⠀⣸⠁⠀⠇⠀⠀⡇⠀⠀⠀⠀⠀⣿⣿⣿
+	⠔⠃⠀⠀⡇⠀⠀⡼⠁⠀⠀⠀⠀⠀⢀⡇⠀⠀⡃⠀⠀⠙⢄⠀⠀⠀⠀⣿⣷⣿
+	⠒⠊⠀⠀⢸⠀⣸⠃⠀⠀⠀⠀⠀⠀⡞⠀⠀⠀⢅⠀⠀⡂⠸⡄⠀⠀⠀⣿⣟⣿
+	⠓⠀⠉⠀⢸⣰⠃⠀⠀⠀⠀⠀⠀⡜⡆⠀⠀⠀⢸⠀⠀⡇⢀⠇⠀⠀⠀⣿⣿⣿
+	⠉⠁⠀⢠⠞⠀⠀⠀⠀⠀⠀⠀⣰⠁⡇⠀⠀⠀⡇⠀⠀⡇⢸⠀⠀⠀⠀⣿⣷⣿
+	⡀⠀⢀⢿⣥⡤⠤⠤⠤⣀⣀⢠⠇⠀⢸⠀⠀⢰⠁⠀⢨⠀⢸⠀⠀⠀⠀⣿⣟⣿
+	
+	Hey There!
+	Don't ask why I'm here.
+	
+	But if you see me, This is the correct
+	module and not a fake one :D
+	
+	By @ImSnox
+	
+	]]
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	local RunService = game:GetService('RunService')
+	local camera = workspace.CurrentCamera
+	local MTREL = "Glass"
+	local binds = {}
+	local root = Instance.new('Folder', camera)
+	root.Name = 'BlurSnox'
+	
+	local gTokenMH = 99999999
+	local gToken = math.random(1, gTokenMH)
+	
+	local DepthOfField = Instance.new('DepthOfFieldEffect', game:GetService('Lighting'))
+	DepthOfField.FarIntensity = 0
+	DepthOfField.FocusDistance = 51.6
+	DepthOfField.InFocusRadius = 50
+	DepthOfField.NearIntensity = 1
+	DepthOfField.Name = "DPT_"..gToken
+	
+	local frame = Instance.new('Frame')
+	frame.Parent = script.Parent
+	frame.Size = UDim2.new(0.95, 0, 0.95, 0)
+	frame.Position = UDim2.new(0.5, 0, 0.5, 0)
+	frame.AnchorPoint = Vector2.new(0.5, 0.5)
+	frame.BackgroundTransparency = 1
+	
+	local GenUid; do -- Generate unique names for RenderStepped bindings
+		local id = 0
+		function GenUid()
+			id = id + 1
+			return 'neon::'..tostring(id)
+		end
+	end
+	
+	do
+		local function IsNotNaN(x)
+			return x == x
+		end
+		local continue = IsNotNaN(camera:ScreenPointToRay(0,0).Origin.x)
+		while not continue do
+			RunService.RenderStepped:wait()
+			continue = IsNotNaN(camera:ScreenPointToRay(0,0).Origin.x)
+		end
+	end
+	
+	local DrawQuad; do
+		local acos, max, pi, sqrt = math.acos, math.max, math.pi, math.sqrt
+		local sz = 0.2
+	
+		function DrawTriangle(v1, v2, v3, p0, p1) -- I think Stravant wrote this function
+			local s1 = (v1 - v2).magnitude
+			local s2 = (v2 - v3).magnitude
+			local s3 = (v3 - v1).magnitude
+			local smax = max(s1, s2, s3)
+			local A, B, C
+			if s1 == smax then
+				A, B, C = v1, v2, v3
+			elseif s2 == smax then
+				A, B, C = v2, v3, v1
+			elseif s3 == smax then
+				A, B, C = v3, v1, v2
+			end
+	
+			local para = ( (B-A).x*(C-A).x + (B-A).y*(C-A).y + (B-A).z*(C-A).z ) / (A-B).magnitude
+			local perp = sqrt((C-A).magnitude^2 - para*para)
+			local dif_para = (A - B).magnitude - para
+	
+			local st = CFrame.new(B, A)
+			local za = CFrame.Angles(pi/2,0,0)
+	
+			local cf0 = st
+	
+			local Top_Look = (cf0 * za).lookVector
+			local Mid_Point = A + CFrame.new(A, B).lookVector * para
+			local Needed_Look = CFrame.new(Mid_Point, C).lookVector
+			local dot = Top_Look.x*Needed_Look.x + Top_Look.y*Needed_Look.y + Top_Look.z*Needed_Look.z
+	
+			local ac = CFrame.Angles(0, 0, acos(dot))
+	
+			cf0 = cf0 * ac
+			if ((cf0 * za).lookVector - Needed_Look).magnitude > 0.01 then
+				cf0 = cf0 * CFrame.Angles(0, 0, -2*acos(dot))
+			end
+			cf0 = cf0 * CFrame.new(0, perp/2, -(dif_para + para/2))
+	
+			local cf1 = st * ac * CFrame.Angles(0, pi, 0)
+			if ((cf1 * za).lookVector - Needed_Look).magnitude > 0.01 then
+				cf1 = cf1 * CFrame.Angles(0, 0, 2*acos(dot))
+			end
+			cf1 = cf1 * CFrame.new(0, perp/2, dif_para/2)
+	
+			if not p0 then
+				p0 = Instance.new('Part')
+				p0.FormFactor = 'Custom'
+				p0.TopSurface = 0
+				p0.BottomSurface = 0
+				p0.Anchored = true
+				p0.CanCollide = false
+				p0.CastShadow = false
+				p0.Material = MTREL
+				p0.Size = Vector3.new(sz, sz, sz)
+				local mesh = Instance.new('SpecialMesh', p0)
+				mesh.MeshType = 2
+				mesh.Name = 'WedgeMesh'
+			end
+			p0.WedgeMesh.Scale = Vector3.new(0, perp/sz, para/sz)
+			p0.CFrame = cf0
+	
+			if not p1 then
+				p1 = p0:clone()
+			end
+			p1.WedgeMesh.Scale = Vector3.new(0, perp/sz, dif_para/sz)
+			p1.CFrame = cf1
+	
+			return p0, p1
+		end
+	
+		function DrawQuad(v1, v2, v3, v4, parts)
+			parts[1], parts[2] = DrawTriangle(v1, v2, v3, parts[1], parts[2])
+			parts[3], parts[4] = DrawTriangle(v3, v2, v4, parts[3], parts[4])
+		end
+	end
+	
+	if binds[frame] then
+		return binds[frame].parts
+	end
+	
+	local uid = GenUid()
+	local parts = {}
+	local f = Instance.new('Folder', root)
+	f.Name = frame.Name
+	
+	local parents = {}
+	do
+		local function add(child)
+			if child:IsA'GuiObject' then
+				parents[#parents + 1] = child
+				add(child.Parent)
+			end
+		end
+		add(frame)
+	end
+	
+	local function UpdateOrientation(fetchProps)
+		local properties = {
+			Transparency = 0.98;
+			BrickColor = BrickColor.new('Institutional white');
+		}
+		local zIndex = 1 - 0.05*frame.ZIndex
+	
+		local tl, br = frame.AbsolutePosition, frame.AbsolutePosition + frame.AbsoluteSize
+		local tr, bl = Vector2.new(br.x, tl.y), Vector2.new(tl.x, br.y)
+		do
+			local rot = 0;
+			for _, v in ipairs(parents) do
+				rot = rot + v.Rotation
+			end
+			if rot ~= 0 and rot%180 ~= 0 then
+				local mid = tl:lerp(br, 0.5)
+				local s, c = math.sin(math.rad(rot)), math.cos(math.rad(rot))
+				local vec = tl
+				tl = Vector2.new(c*(tl.x - mid.x) - s*(tl.y - mid.y), s*(tl.x - mid.x) + c*(tl.y - mid.y)) + mid
+				tr = Vector2.new(c*(tr.x - mid.x) - s*(tr.y - mid.y), s*(tr.x - mid.x) + c*(tr.y - mid.y)) + mid
+				bl = Vector2.new(c*(bl.x - mid.x) - s*(bl.y - mid.y), s*(bl.x - mid.x) + c*(bl.y - mid.y)) + mid
+				br = Vector2.new(c*(br.x - mid.x) - s*(br.y - mid.y), s*(br.x - mid.x) + c*(br.y - mid.y)) + mid
+			end
+		end
+		DrawQuad(
+			camera:ScreenPointToRay(tl.x, tl.y, zIndex).Origin, 
+			camera:ScreenPointToRay(tr.x, tr.y, zIndex).Origin, 
+			camera:ScreenPointToRay(bl.x, bl.y, zIndex).Origin, 
+			camera:ScreenPointToRay(br.x, br.y, zIndex).Origin, 
+			parts
+		)
+		if fetchProps then
+			for _, pt in pairs(parts) do
+				pt.Parent = f
+			end
+			for propName, propValue in pairs(properties) do
+				for _, pt in pairs(parts) do
+					pt[propName] = propValue
+				end
+			end
+		end
+	end
+	
+	UpdateOrientation(true)
+	RunService:BindToRenderStep(uid, 2000, UpdateOrientation)
 end
+coroutine.wrap(PPQLU_fake_script)()
+local function NJHNB_fake_script() -- BlurTextDark.Animation 
+	local script = Instance.new('LocalScript', BlurTextDark)
 
-createTextLabel(Frame, UDim2.new(0.230, 0, 0.536, 0), UDim2.new(0, 347, 0, 55), "Alsolara Loader")
-createTextLabel(Frame, UDim2.new(0.231, 0, 0.680, 0), UDim2.new(0, 347, 0, 27), "Fixes are on the way!")
-createTextLabel(Frame, UDim2.new(0.231, 0, 0.760, 0), UDim2.new(0, 347, 0, 17), "cr1tcal3 issued a FIX post")
-
-local UICorner = Instance.new("UICorner", Frame)
-
-local function createImageLabel(parent, position, size, imageId)
-    local imgLabel = Instance.new("ImageLabel", parent)
-    imgLabel.BackgroundTransparency = 1
-    imgLabel.Position = position
-    imgLabel.Size = size
-    imgLabel.Image = imageId
-    imgLabel.ScaleType = Enum.ScaleType.Crop
-    return imgLabel
+	function twui(element, type, tweenType, easingDirection, tweenDuration, tweenGoal, tweenEndGoal)
+		local tweenInfo = TweenInfo.new(tweenDuration, Enum.EasingStyle[tweenType], Enum.EasingDirection[easingDirection])
+		local tweenProperties = {}
+	
+		if type == "Position" then
+			tweenProperties.Position = tweenGoal
+		elseif type == "Size" then
+			tweenProperties.Size = tweenGoal
+		elseif type == "Rotation" then
+			tweenProperties.Rotation = tweenGoal
+		elseif type == "Transparency" then
+			tweenProperties.BackgroundTransparency = tweenGoal
+		elseif type == "ImageTransparency" then
+			tweenProperties.ImageTransparency = tweenGoal
+		end
+	
+		local tween = game:GetService("TweenService"):Create(element, tweenInfo, tweenProperties)
+		tween:Play()
+		tween.Completed:Wait()
+	
+		if type == "Position" then
+			tweenProperties.Position = tweenEndGoal
+		elseif type == "Size" then
+			tweenProperties.Size = tweenEndGoal
+		elseif type == "Rotation" then
+			tweenProperties.Rotation = tweenEndGoal
+		elseif type == "Transparency" then
+			tweenProperties.BackgroundTransparency = tweenEndGoal
+		elseif type == "ImageTransparency" then
+			tweenProperties.ImageTransparency = tweenEndGoal
+		end
+	
+		local endTween = game:GetService("TweenService"):Create(element, tweenInfo, tweenProperties)
+		endTween:Play()
+	end
+	
+	wait(0.3)
+	twui(script.Parent, "Position", "Quint", "InOut", 3, UDim2.new(-1,0,0,0), UDim2.new(0,0,0,0))
+	
 end
-
-local ImageLabel = createImageLabel(Frame, UDim2.new(0, 0, 0.018, 0), UDim2.new(0, 646, 0, 145), "rbxassetid://18940309419")
-local UIGradient = Instance.new("UIGradient", ImageLabel)
-UIGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0, Color3.fromRGB(255, 255, 255)), ColorSequenceKeypoint.new(1, Color3.fromRGB(0, 0, 0))}
-UIGradient.Rotation = 90
-
-createImageLabel(Frame, UDim2.new(0, 0, 0.452, 0), UDim2.new(0, 646, 0, 37), "rbxassetid://5012875997")
-
-local fluency_icon = Instance.new("ImageLabel", fix)
-fluency_icon.AnchorPoint = Vector2.new(0.5, 0.5)
-fluency_icon.BackgroundTransparency = 1
-fluency_icon.Position = UDim2.new(0.5, 0, 0.5, 0)
-fluency_icon.Size = UDim2.new(0, 208, 0, 91)
-fluency_icon.Image = "rbxassetid://12975609170"
-fluency_icon.ScaleType = Enum.ScaleType.Fit
-
-createTextLabel(fluency_icon, UDim2.new(-0.335, 0, 0.911, 0), UDim2.new(0, 347, 0, 27), "Liquid Software")
-createTextLabel(fluency_icon, UDim2.new(0.141, 0, 1.208, 0), UDim2.new(0, 150, 0, 18), "Org by Cr1tcal3")
-
-local function fadeGui(guiElement, duration, targetTransparency)
-    local tween = game:GetService("TweenService"):Create(guiElement, TweenInfo.new(duration, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {BackgroundTransparency = targetTransparency})
-    tween:Play()
-    tween.Completed:Wait()
-end
-
-local function KQRVQP_fake_script()
-    local boot = script.Parent.boot
-    
-    fluency_icon.Visible = true
-    fadeGui(fluency_icon, 0.5, 0)
-    wait(3.402)
-    fadeGui(fluency_icon, 0.5, 1)
-    Frame.Visible = true
-    fadeGui(Frame, 0.5, 0)
-end
-
-coroutine.wrap(KQRVQP_fake_script)()
+coroutine.wrap(NJHNB_fake_script)()
